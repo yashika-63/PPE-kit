21ddd34d9a88d906257ad35c61b9e57a845bea04
yashika-63 <yashika.patil@pristine-code.com>
2025-12-12 11:21:18 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -360,4 +360,4 @@ def get_analytics():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=3005, threaded=True)
